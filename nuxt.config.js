8169b944e0d9fdb2b1cd29197f92d81086030c32
Aryo <98627850+aryo-sr@users.noreply.github.com>
2023-07-14 02:05:13 +0700

    Set website name and base url in env var

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,23 +8,27 @@ export default {
   globalName: 'aryos',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'aryos.id',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
+  head() {
+    const path = this.$route ? process.env.BASE_URL + this.$route.path : process.env.BASE_URL
+    return {
+      title: process.env.WEBSITE_NAME,
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'canonical', href: path }
+      ],
       bodyAttrs: {
-          class: 'app'
+        class: 'app'
       }
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
